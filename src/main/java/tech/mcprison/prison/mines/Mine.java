be1c638b298dc8cce8d9e4a78a6dfd5eb984fc6d
SirFaizdat <fdatoo7@gmail.com>
2017-04-16 17:04:11 -0400

    This is Public Beta 1

--- src/main/java/tech/mcprison/prison/mines/Mine.java
+++ src/main/java/tech/mcprison/prison/mines/Mine.java
@@ -182,7 +182,6 @@ public class Mine {
                     }
                 }
             }
-            Output.get().logInfo("Reset mine " + name);
             if (PrisonMines.get().getConfig().asyncReset) {
                 asyncGen();
             }
